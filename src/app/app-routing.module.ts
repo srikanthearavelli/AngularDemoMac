497170cd72c09c86a0121db6e8ed1f492c837e9d
srikanth <srikanth@Sindhujas-MacBook-Air.local>
2025-08-02 21:12:13 -0400

    Resolve merge conflicts: keep local changes for Angular config, routing, and package files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
@@ -9,6 +10,10 @@ export const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: '**', redirectTo: '/login' }
 ];
+=======
+
+export const routes: Routes = [];
+>>>>>>> master
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
